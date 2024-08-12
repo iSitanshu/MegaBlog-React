b7b4f0c00e5e3abd4831137e073af964e8d250e8
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-13 04:29:16 +0530

    Services

---
+++ Front/src/appwrite/auth.js
@@ -0,0 +1,68 @@
+import conf from '../conf/conf'
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client()
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                //call another method
+
+            }else{
+                return userAccount;
+            }
+        }catch(error){
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try{
+            await this.account.createEmailPasswordSession(email, password);
+            if(userAccount){
+                return this.login({email, password})
+            }else{
+                return userAccount;
+            }
+        }catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get()
+        }catch(error){
+            // throw error;
+            console.log("Appwrite serive :: getcurrentuser :: error ",error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try{
+            if(userAccount){
+                await this.account.deleteSessions()
+            }else{
+                return userAccount
+            }
+        }catch(error){
+            console.log("Appwrite serive :: logout :: error ",error);
+            
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default AuthService
