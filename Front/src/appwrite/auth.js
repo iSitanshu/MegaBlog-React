7857ede5b9f08a9cca80c63acbb0a8cb89ceff3b
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-16 15:47:00 +0530

    Update auth

--- Front/src/appwrite/auth.js
+++ Front/src/appwrite/auth.js
@@ -17,7 +17,7 @@ export class AuthService {
             const userAccount = await this.account.create(ID.unique(), email, password, name)
             if (userAccount) {
                 //call another method
-
+                this.login({email,password})
             }else{
                 return userAccount;
             }
