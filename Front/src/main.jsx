d4a174e0f76e700a97f2b7cd5f57a07730596034
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-20 23:14:50 +0530

    Update main

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -4,10 +4,13 @@ import './index.css'
 import App from './App'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
